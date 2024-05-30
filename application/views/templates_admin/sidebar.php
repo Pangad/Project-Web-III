322e71e07aef6f80ad276615c3d38e8a84133523
PangadEko <pangadekoputra26@gmail.com>
2024-05-30 19:34:36 +0700

    membuat invoice penjualan

--- application/views/templates_admin/sidebar.php
+++ application/views/templates_admin/sidebar.php
@@ -33,7 +33,7 @@
                     <span>Data Barang</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url('admin/invoices') ?>">
+                <a class="nav-link" href="<?php echo base_url('admin/invoice') ?>">
                     <i class="fas fa-fw fa-file-invoice"></i>
                     <span>Invoices</span></a>
             </li>
